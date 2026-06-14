8c8b7b93605d3e88d0025191636714a80fcbc39f
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2026-06-14 13:52:05 +0400

    first commit

---
+++ routes/console.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Jobs\GenerateDailyReservationSummary;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    GenerateDailyReservationSummary::dispatch();
+})->everyMinute();
